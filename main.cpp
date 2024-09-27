83cfccfad5b9104a4403b3ce3c81551a7517fb00
SantiagoAndradeUDLA <santiago.andrade.bucheli@udla.edu.ec>
2024-09-26 19:46:58 -0500

    calculadora_comoenclase

--- main.cpp
+++ main.cpp
@@ -1,17 +1,48 @@
-//CALCULADORA EN C
+#include <stdio.h>
 
-#include <iostream>
-#include <stdlib.h>
-#include <math.h>
+int main (int argc, char *argv[]) {
 
-using namespace std;
+    double num1, num2, sum, res, mul, div;
+    int op1=0, op2=0;
 
-int main()
-{
-    int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
-    cout << "CALCULADORA EN C++" << endl;
+    do{
+    printf("Enter num1: ");
+    scanf("%lf",&num1);
+    printf("Enter num2: ");
+    scanf("%lf",&num2);
 
-    return 0;   
+    printf("Select Operation:\n1.Sum\n2.Substraction\n3.Multiplication\n4.Division\n>>");
+    scanf("%d",&op1);
+
+    switch (op1)
+    {
+    case 1:
+        sum=num1+num2;
+        printf("The sum is: %lf\n",sum);
+        break;
+    case 2:
+        res=num1-num2;
+        printf("Substration is: %lf\n",res);
+        break;
+    case 3:
+        mul=num1*num2;
+        printf("Multiplicaction is: %lf\n",mul);
+        break;
+    case 4:
+        if(num2!=0){
+            div=num1/num2;
+            printf("Division is: %lf\n",div);
+        }else{
+            printf("Can not divide 0\n");
+        }
+        break;
+    default:
+        printf("No option selected\n");
+        break;
+    }
+    printf("Would you like to make another operation?\n1.Y\n2.N\n>>");
+    scanf("%d",&op2);
+    }while(op2==1);
+
+    return 0;
 }
-
-
